05cd2cb883ea239db2caa99f9e7382ceda87daea
Vlad Didenko <git@didenko.com>
2017-07-22 15:53:03 -0500

    Add Collector interface use to Common function

--- common.go
+++ common.go
@@ -9,6 +9,13 @@ package slops // import "didenko.com/go/slops"
 // a new slice. The order is preserved. The lesser number of
 // duplicates is preserved
 func Common(left, right []string) []string {
+	return CollectCommon(left, right, &getAll{})
+}
+
+// CollectCommon applies a Collector to every item which is
+// in both left and right slices. Both input slices are
+// expected to be sorted.
+func CollectCommon(left, right []string, c Collector) []string {
 
 	common := make([]string, 0)
 
@@ -23,7 +30,7 @@ func Common(left, right []string) []string {
 			continue
 		}
 
-		common = append(common, left[i])
+		common = c.Collect(common, left[i])
 		i++
 		j++
 	}
