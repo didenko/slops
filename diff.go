15595c413203722d56a2763f8543833a4fa6a04d
Vlad Didenko <git@didenko.com>
2017-07-23 08:54:56 -0500

    Fix the index bug in Diff,  clarified handling dups

--- diff.go
+++ diff.go
@@ -4,7 +4,9 @@
 package slops // import "didenko.com/go/slops"
 
 // Diff takes sorted slices of strings and returns the non-
-// overlapping entries. The result is still sorted.
+// overlapping entries. The result is still sorted. See
+// `CollectDifferent` documentation for handling duplicates
+// in the input slices
 func Diff(left, right []string) (leftOnly, rightOnly []string) {
 	ga := &getAll{}
 	return CollectDifferent(left, right, ga, ga)
@@ -13,6 +15,12 @@ func Diff(left, right []string) (leftOnly, rightOnly []string) {
 // CollectDifferent applies related Collectors to every item which is
 // in one slice (left or right), but not in the other (right or left)
 // slice. Both input slices are expected to be sorted.
+//
+// Smallest number of duplicates ignored in each side. E.g. if
+// the pseudo inputs are {{"-"}, {"-", "-"}}, then the pseudo
+// output will be {{}, {Collect("-")}}. In other words,
+// Collector will be invoked on an excess number of duplicate
+// items
 func CollectDifferent(left, right []string, lc, rc Collector) (leftColl, rightColl []string) {
 
 	leftColl = make([]string, 0)
@@ -20,13 +28,13 @@ func CollectDifferent(left, right []string, lc, rc Collector) (leftColl, rightCo
 
 	for l, r := 0, 0; l < len(left) || r < len(right); {
 
-		if l == len(left) || left[l] > right[r] {
+		if r < len(right) && (l == len(left) || left[l] > right[r]) {
 			rightColl = rc.Collect(rightColl, right[r])
 			r++
 			continue
 		}
 
-		if r == len(right) || left[l] < right[r] {
+		if l < len(left) && (r == len(right) || left[l] < right[r]) {
 			leftColl = lc.Collect(leftColl, left[l])
 			l++
 			continue
--- diff_test.go
+++ diff_test.go
@@ -9,47 +9,39 @@ import (
 )
 
 type diffUseCase struct {
-	ladd      bool
-	left      string
-	radd      bool
-	right     string
-	leftOnly  []string
-	rightOnly []string
+	left      []string
+	right     []string
+	onlyLeft  []string
+	onlyRight []string
 }
 
 var diffTestScript = []diffUseCase{
-	{false, "", false, "", []string{}, []string{}},
-	{false, "", true, "", []string{}, []string{""}},
-	{true, "", false, "", []string{}, []string{}},
-	{true, "", true, "a", []string{""}, []string{"a"}},
-	{true, "a", false, "", []string{""}, []string{}},
-	{true, "b", true, "b", []string{""}, []string{}},
-	{false, "", true, "c", []string{""}, []string{"c"}},
-	{false, "", true, "d", []string{""}, []string{"c", "d"}},
+	{nil, nil, []string{}, []string{}},
+	{[]string{}, nil, []string{}, []string{}},
+	{[]string{}, []string{}, []string{}, []string{}},
+	{[]string{}, []string{"-"}, []string{}, []string{"-"}},
+	{[]string{"-"}, []string{"-"}, []string{}, []string{}},
+	{[]string{"-", "-"}, []string{"-"}, []string{"-"}, []string{}},
+	{[]string{"-", "-", "-"}, []string{"-"}, []string{"-", "-"}, []string{}},
+	{[]string{"-", "-"}, []string{"-", "a"}, []string{"-"}, []string{"a"}},
+	{[]string{"-", "-", "a"}, []string{"-", "a"}, []string{"-"}, []string{}},
+	{[]string{"-", "-", "a", "b"}, []string{"-", "a", "b"}, []string{"-"}, []string{}},
+	{[]string{"-", "-", "b"}, []string{"-", "a", "b"}, []string{"-"}, []string{"a"}},
+	{[]string{"-", "b"}, []string{"-", "a", "b"}, []string{}, []string{"a"}},
+	{[]string{"-", "b"}, []string{"-", "a", "b", "c"}, []string{}, []string{"a", "c"}},
 }
 
 func TestDiff(t *testing.T) {
-	left := make([]string, 0)
-	right := make([]string, 0)
-
 	for uci, uc := range diffTestScript {
 
-		if uc.ladd {
-			left = append(left, uc.left)
+		lo, ro := Diff(uc.left, uc.right)
+
+		if !reflect.DeepEqual(lo, uc.onlyLeft) {
+			t.Error("At index", uci, "left excess", lo, "does not match expected", uc.onlyLeft)
 		}
 
-		if uc.radd {
-			right = append(right, uc.right)
-		}
-
-		lo, ro := Diff(left, right)
-
-		if !reflect.DeepEqual(lo, uc.leftOnly) {
-			t.Error("At index", uci, "left excess", lo, "does not match expected", uc.leftOnly)
-		}
-
-		if !reflect.DeepEqual(ro, uc.rightOnly) {
-			t.Error("At index", uci, "right excess", ro, "does not match expected", uc.rightOnly)
+		if !reflect.DeepEqual(ro, uc.onlyRight) {
+			t.Error("At index", uci, "right excess", ro, "does not match expected", uc.onlyRight)
 		}
 	}
 }
