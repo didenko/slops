7e584ee8ebe26449a5ac1a43ee0a3d04a294de76
Vlad Didenko <git@didenko.com>
2017-07-31 23:25:22 -0500

    Added the generic slice collector for all variants of overlap.

---
+++ variety.go
@@ -0,0 +1,36 @@
+// Copyright 2017 Vlad Didenko. All rights reserved.
+// See the included LICENSE.md file for licensing information
+
+package slops // import "didenko.com/go/slops"
+
+// CollectVariety applies a Collector to every item in left and
+// right slices. Common items are collected as many times as
+// there are common occurences by the related collector. Non-common
+// items each, with their duplicates which are extra to the common
+// items, are collected by related leftOnly and rightOnly Collectors.
+// Both left and right input slices are expected to be sorted.
+func CollectVariety(left, right []string, onlyLeft, common, onlyRight Collector) []string {
+
+	collected := make([]string, 0)
+
+	for l, r := 0, 0; l < len(left) || r < len(right); {
+
+		if l < len(left) && (r == len(right) || left[l] < right[r]) {
+			collected = onlyLeft(collected, left[l])
+			l++
+			continue
+		}
+
+		if r < len(right) && (l == len(left) || left[l] > right[r]) {
+			collected = onlyRight(collected, right[r])
+			r++
+			continue
+		}
+
+		collected = common(collected, left[l])
+		l++
+		r++
+	}
+
+	return collected
+}
---
+++ variety_test.go
@@ -0,0 +1,46 @@
+// Copyright 2017 Vlad Didenko. All rights reserved.
+// See the included LICENSE.md file for licensing information
+
+package slops // import "didenko.com/go/slops"
+
+import (
+	"reflect"
+	"testing"
+)
+
+type varietyUseCase struct {
+	left   []string
+	right  []string
+	expect []string
+}
+
+var varietyTestScript = []varietyUseCase{
+	{[]string{}, []string{}, []string{}},
+
+	{[]string{"a"}, []string{}, []string{"lo:a"}},
+	{[]string{"a"}, []string{"b"}, []string{"lo:a", "ro:b"}},
+	{[]string{"a", "b"}, []string{"b", "c"}, []string{"lo:a", "cm:b", "ro:c"}},
+
+	{[]string{}, []string{"a"}, []string{"ro:a"}},
+	{[]string{"b"}, []string{"a"}, []string{"ro:a", "lo:b"}},
+	{[]string{"b", "c"}, []string{"a", "b"}, []string{"ro:a", "cm:b", "lo:c"}},
+
+	{[]string{"a", "a", "a"}, []string{"-", "a", "a"}, []string{"ro:-", "cm:a", "cm:a", "lo:a"}},
+	{[]string{"-", "a", "a"}, []string{"a", "a", "a"}, []string{"lo:-", "cm:a", "cm:a", "ro:a"}},
+}
+
+func addLabel(label string) Collector {
+	return func(pile []string, item string) []string {
+		return append(pile, label+item)
+	}
+}
+
+func TestVariety(t *testing.T) {
+	for uci, uc := range varietyTestScript {
+		result := CollectVariety(uc.left, uc.right, addLabel("lo:"), addLabel("cm:"), addLabel("ro:"))
+
+		if !reflect.DeepEqual(uc.expect, result) {
+			t.Error("At index", uci, "result", result, "does not match expected", uc.expect)
+		}
+	}
+}
